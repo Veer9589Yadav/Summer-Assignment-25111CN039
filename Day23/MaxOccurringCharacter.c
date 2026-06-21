83dfd0bdd9ce9889a739f6d9c6c10347210f0ac4
Veer9589Yadav <py023732@gmail.com>
2026-06-21 12:49:58 +0530

    Day-23 Uploaded

---
+++ Day23/MaxOccurringCharacter.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    char str[100];
+    int freq[256]= {0};
+    char maxChar;
+    int maxFreq= 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    // Count frequency of each character
+    for (int i= 0; str[i] != '\0'; i++) {
+        freq[(unsigned char)str[i]]++;
+    }
+    // Find maximum occurring character
+    for (int i= 0; i< 256; i++) {
+        if (freq[i]> maxFreq) {
+            maxFreq= freq[i];
+            maxChar= i;
+        }
+    }
+    printf("Maximum occurring character: %c\n", maxChar);
+    printf("Frequency: %d\n", maxFreq);
+    return 0;
+}
